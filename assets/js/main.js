62bcbb041f402ffe27bc9a3f02f09493acbdc1a7
levybooth <dlevybooth@gmail.com>
2016-02-13 22:09:13 -0800

    mohn lab website

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Nav.
+			$('#nav')
+				.append('<a href="#nav" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+	});
+
+})(jQuery);
